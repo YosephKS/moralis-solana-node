e2c0f58c0665c3f954e61d34398619b4703f30c4
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2022-07-21 21:59:05 +0700

    adding finished version of the tutorial

--- README.md
+++ README.md
@@ -1,2 +1,33 @@
-# moralis-solana-node
-Demo on Moralis Solana API in Node.js
+# Solana API on Express (Node.js)
+
+Demo on Moralis Solana API in Express app.
+
+## Get Started
+
+### 1. Install dependencies
+
+```sh
+# For NPM
+npm i
+
+# For Yarn
+yarn
+```
+
+### 2. Add enviornment variables
+
+Copy `.env.example` and rename the new file `.env`, then fill in the following variables:
+
+```
+MORALIS_DAPP_URL=xxx
+MORALIS_APP_ID=xxx
+MORALIS_SOLANA_API_KEY=xxx
+```
+
+These can be fetched from Moralis Admin Dashboard, if you haven't had an account create one at https://admin.moralis.io
+
+### 3. Run Express app
+
+```sh
+yarn start
+```
--- app.js
+++ app.js
@@ -1,11 +1,44 @@
+const axios = require("axios");
 const express = require("express");
+const Moralis = require("moralis/node");
+require("dotenv").config();
 const app = express();
 const port = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+/**
+ * @description Using Moralis SDK to call Solana API
+ */
+app.get("/sdk", async (_, res) => {
+  await Moralis.start({
+    serverUrl: process.env.MORALIS_DAPP_URL,
+    appId: process.env.MORALIS_APP_ID,
+  });
+  // get devnet SOL balance for a given address
+  const options = {
+    network: "devnet",
+    address: "6XU36wCxWobLx5Rtsb58kmgAJKVYmMVqy4SHXxENAyAe",
+  };
+  const solBalance = await Moralis.SolanaAPI.account.balance(options);
+  res.status(200).send(solBalance);
+});
+
+/**
+ * @description Using REST API to call Solana API
+ */
+app.get("/rest", async (_, res) => {
+  const { data } = await axios.get(
+    "https://solana-gateway.moralis.io/account/devnet/6XU36wCxWobLx5Rtsb58kmgAJKVYmMVqy4SHXxENAyAe/balance",
+    {
+      headers: {
+        "X-API-Key": process.env.MORALIS_SOLANA_API_KEY,
+      },
+    }
+  );
+  res.status(200).send(data);
 });
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+  console.log(
+    `Moralis Solana Express app listening on http://localhost:${port} 🚀`
+  );
 });
